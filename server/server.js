79f7af6036b85c0f7afff88f8abaec3cb3ca1690
JFiedler23 <43707498+JFiedler23@users.noreply.github.com>
2021-02-01 15:24:45 -0600

    first commit

--- server/server.js
+++ server/server.js
@@ -49,9 +49,9 @@ app.use("/api/games", gamesRoutes);
 //serve static assets if in production
 if(process.env.NODE_ENV === "production"){
     //set static folder
-    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+    app.use(express.static(path.resolve(__dirname, '../', 'client', 'build')));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'));
     });
 }
 
