88ae203916990996d1ec0c40cf04b0dac8f62584
JFiedler23 <43707498+JFiedler23@users.noreply.github.com>
2021-02-01 17:09:35 -0600

    Update keys.js

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,6 +1,4 @@
 module.exports = {
     mongoURI: "mongodb+srv://gamechest:GB4OW3rkELph5vrc@main-cluster.pdtk5.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    secretOrKey: "spicy",
-    gamesDbPrivate: "e41d534bf705e3455f3633fe718df32a2cca55a8cc97ca0b481bc5b2ea4e83bd",
-    gamesDbPublic: "ddaa1392929797f6cb6327d2edc11da36ddba1453671ad2eccda872b7f1f65df"
+    secretOrKey: "spicy"
 }
