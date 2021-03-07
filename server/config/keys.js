37564adc5208cfffac22908c2b1cefcba68ebcd1
JFiedler23 <43707498+JFiedler23@users.noreply.github.com>
2021-03-07 16:41:23 -0600

    Update keys.js

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
+//require('dotenv').config();
 
 module.exports = {
-    mongoURI: process.env.MONGO_URI,
-    secretOrKey: process.env.SECRET
+    mongoURI: "mongodb+srv://gamechest:GB4OW3rkELph5vrc@main-cluster.pdtk5.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    secretOrKey: secret
 }
